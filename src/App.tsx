e5215f5cb744c0dcad597a72aa343bdfcfe93207
Aleena <toaleenavshaji@gmail.com>
2025-01-07 09:43:56 +0530

    header-component is maded

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import HeaderPart from "./components/header";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen overflow-hidden flex flex-col">
+        <HeaderPart />
+      </div>
+    </>
+  );
+}
+
+export default App;
