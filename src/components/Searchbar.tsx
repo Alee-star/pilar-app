6fbc56f4caffa7178f4afc214e70da42adc36b24
Aleena <toaleenavshaji@gmail.com>
2025-01-08 15:20:46 +0530

    fix: clsx is removed

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 import { SearchProps, SearchVarient } from "../types/SearchTypes";
 
 const Searchbar: React.FC<SearchProps> = ({
@@ -9,21 +8,19 @@ const Searchbar: React.FC<SearchProps> = ({
   varient = SearchVarient.PRIMARY,
   onChange,
 }) => {
+  const inputClass =
+    varient === SearchVarient.PRIMARY
+      ? "block w-full rounded-lg pl-10 p-2.5 text-gray-900 text-sm"
+      : "block w-full rounded-lg pl-10 p-2.5 text-gray-900 text-sm bg-white outline-none";
+
   return (
     <div className="flex">
       <div className="relative w-full">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-          <img src={image} alt={imageAlt}></img>
+          <img src={image} alt={imageAlt} />
         </div>
         <input
-          className={clsx(
-            "block w-full rounded-lg pl-10 p-2.5 text-gray-900 text-sm",
-            {
-              "border bg-gray-50 border-gray-300":
-                varient === SearchVarient.PRIMARY,
-              "bg-white outline-none": varient === SearchVarient.SECONDARY,
-            }
-          )}
+          className={inputClass}
           placeholder={placeholder}
           onChange={(e) => {
             console.log("Search:", e.target.value);
