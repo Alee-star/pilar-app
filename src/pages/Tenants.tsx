6681c1391adb02625174551e7508f045225888ce
Aleena <toaleenavshaji@gmail.com>
2025-01-17 10:05:53 +0530

    feat: add heading and button

--- src/pages/Tenants.tsx
+++ src/pages/Tenants.tsx
@@ -4,6 +4,8 @@ import Searchbar from "../components/Searchbar";
 import ApartmentSelector from "../components/ApartmentSelector";
 import Layout from "../pages/Layout";
 import FileUploader from "../components/FileUploader";
+import Button from "../components/Button";
+import { ButtonVarient } from "../types/ButtonTypes";
 
 const Tenants = () => {
   const [selectedView, setSelectedView] = useState("");
@@ -12,10 +14,23 @@ const Tenants = () => {
     <Layout setSelectedView={setSelectedView}>
       {selectedView === "Tenants" ? (
         <>
-          <div className="flex overflow-hidden w-full gap-3 mb-6">
-            <Searchbar placeholder="Search by User" onChange={() => {}} />
-            <DatePicker placeholder="mm/dd/yyyy" onChange={() => {}} />
-            <ApartmentSelector label="Apartment" onChange={() => {}} />
+          <div className="flex flex-col gap-3 mb-6">
+            <div className="flex justify-between items-center flex-wrap gap-2">
+              <h3 className="font-semibold text-3xl leading-10 text-gray-900 mt-[1]">
+                Tenants
+              </h3>
+              <Button
+                label="Add New Tenant"
+                varient={ButtonVarient.SECONDARY}
+                imageAlt="plus-icon"
+                image="/assets/plus.svg"
+              />
+            </div>
+            <div className="flex items-center gap-2 flex-wrap">
+              <Searchbar placeholder="Search by User" onChange={() => {}} />
+              <DatePicker placeholder="mm/dd/yyyy" onChange={() => {}} />
+              <ApartmentSelector label="Apartment" onChange={() => {}} />
+            </div>
           </div>
           <div className="flex w-full mt-10 ml-3">
             <FileUploader
